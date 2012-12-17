84dea9c858cc2439f1e314e193947674842e16c0
dongzhu-yin <dongzhu.yin@connecty.co.jp>
2012-12-17 15:21:36 +0900

    init commit

---
+++ config/core.el
@@ -0,0 +1,13 @@
+;; Here is the root of your personal configs.
+;; Either place config straight in here,
+;; such as this colour theme (feel free to change it to your own favourite theme)
+
+;; use blackbored colour theme
+(load-file (concat dotfiles-lib-dir "blackbored.el"))
+(color-theme-blackbored)
+
+;;Or load external files such as this bindings file:
+(load-dotfile "config/bindings.el")
+
+(require 'multi-term)
+(setq multi-term-program "/bin/bash")
